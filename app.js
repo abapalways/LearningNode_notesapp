0b31d3b2d371c86ef723d4a240a3339337644b51
abapalways <avanishjoshi@gmail.com>
2024-02-29 15:35:17 -0600

    added template

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
-const fs = require('fs')
+const yargs = require('yargs')
+//const notes = require('./notes.js')
 
-fs.writeFileSync('notes.txt', 'This ile was created using Node.js!' )
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note!')
+    }
+
+})
+
+
+yargs.command({
+    command: 'list',
+    describe: 'list a note',
+    handler: function() {
+        console.log('List Note!')
+    }
+
+})
+
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note!')
+    }
+
+})
+
+yargs.parse()
